1a87ad964ecdef0eeb14ef8c4ece272699b0ab2a
Airsick <corauh@umflint.edu>
2017-04-06 14:29:20 -0400

    whatup doe

--- app/src/main/java/stone/philosophers/com/driversed/TeacherLanding.java
+++ app/src/main/java/stone/philosophers/com/driversed/TeacherLanding.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,6 +16,7 @@ public class TeacherLanding extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    protected Button mapButton;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,6 +33,15 @@ public class TeacherLanding extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mapButton = (Button) findViewById(R.id.mapButton);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TeacherLanding.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
