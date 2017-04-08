1c92d934166ef7d2a9941517583ebfaea97cb6ae
ajpowell021 <ajpowell021@gmail.com>
2017-04-08 00:47:29 -0400

    Add image upload from gallery.

--- app/src/main/java/stone/philosophers/com/driversed/UploadDrive.java
+++ app/src/main/java/stone/philosophers/com/driversed/UploadDrive.java
@@ -1,14 +1,27 @@
 package stone.philosophers.com.driversed;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 
 // After a drive has been completed, upload the info to the database.
 // Also uploads an image if wanted.
@@ -17,8 +30,13 @@ public class UploadDrive extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private StorageReference mStorage;
+
+    private static final int GALLERY_INTENT = 2;
+    private ProgressDialog uploadProgressDialog;
 
     private EditText milesDrivenEditText;
+    private Button selectImageButton;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,12 +50,48 @@ public class UploadDrive extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        uploadProgressDialog = new ProgressDialog(this);
 
         // Find UI Elements
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
 
         milesDrivenEditText = (EditText) findViewById(R.id.milesTextView);
+        selectImageButton = (Button) findViewById(R.id.selectImageButton);
+
+        // On click listeners
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+            uploadProgressDialog.setMessage("Uploading ... ");
+            uploadProgressDialog.show();
+
+            Uri uri = data.getData();
+
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(UploadDrive.this, "Upload Done", Toast.LENGTH_LONG).show();
+                    uploadProgressDialog.dismiss();
+                }
+            });
+        }
     }
 
     @Override
